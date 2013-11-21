0a722310e2f8a1c80df6f988531daf73a2ee2398
William Throwe <wtt6@cornell.edu>
2013-11-20 22:28:10 -0500

    Allow choosing stepper at runtime

--- EvolveCluster.cpp
+++ EvolveCluster.cpp
@@ -1,5 +1,6 @@
 #include "FreeInteractionStepper.hpp"
 #include "LeapfrogStepper.hpp"
+#include "ModifiedEulerStepper.hpp"
 #include "ReadCluster.hpp"
 #include "SuperstarCluster.hpp"
 #include "WriteCluster.hpp"
@@ -7,24 +8,45 @@
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-  if(argc != 4) {
-    std::cerr << "Usage: EvolveCluster file t dt\n";
+  bool use_leapfrog = false;
+  {
+    int opt;
+    while((opt = getopt(argc, argv, "l")) != -1) {
+      switch(opt) {
+      case 'l':
+	use_leapfrog = true;
+	break;
+      default:
+	std::cerr << "Usage: EvolveCluster [-l] file t dt\n";
+	return 1;
+      }
+    }
+  }
+
+  if(argc - optind != 3) {
+    std::cerr << "Usage: EvolveCluster [-l] file t dt\n";
     return 1;
   }
 
-  const double tmax = atof(argv[2]);
-  const double dt = atof(argv[3]);
+  const double tmax = atof(argv[optind+1]);
+  const double dt = atof(argv[optind+2]);
   const unsigned nsteps = static_cast<unsigned>(round(tmax/dt));
 
   SuperstarCluster cluster(1);
-  ReadCluster(cluster, argv[1]);
+  ReadCluster(cluster, argv[optind]);
 
-  Steppers::Leapfrog stepper(new Steppers::Free, new Steppers::Interaction);
+  Stepper *const stepper = use_leapfrog ?
+    static_cast<Stepper*>(new Steppers::Leapfrog(new Steppers::Free,
+						 new Steppers::Interaction)) :
+    static_cast<Stepper*>(new
+			  Steppers::ModifiedEuler(new Steppers::Free,
+						  new Steppers::Interaction));
 
   for(unsigned step=0;step<nsteps;step++) {
-    stepper.Step(cluster, dt);
+    stepper->Step(cluster, dt);
   }
   cluster.PrepareStep();
   std::cout.precision(16);
--- TestEnergy.cpp
+++ TestEnergy.cpp
@@ -1,10 +1,12 @@
 #include "FreeInteractionStepper.hpp"
 #include "LeapfrogStepper.hpp"
+#include "ModifiedEulerStepper.hpp"
 #include "ReadCluster.hpp"
 #include "SuperstarCluster.hpp"
 
 #include <cstdlib>
 #include <iostream>
+#include <unistd.h>
 
 void PrintCluster(double t, const SuperstarCluster& cluster) {
   std::cout << t << "\t" << cluster.Energy();
@@ -18,19 +20,39 @@ void PrintCluster(double t, const SuperstarCluster& cluster) {
 }
 
 int main(int argc, char **argv) {
-  if(argc != 4) {
-    std::cerr << "Usage: TestEnergy dt nsteps file\n";
+  bool use_leapfrog = false;
+  {
+    int opt;
+    while((opt = getopt(argc, argv, "l")) != -1) {
+      switch(opt) {
+      case 'l':
+	use_leapfrog = true;
+	break;
+      default:
+	std::cerr << "Usage: TestEnergy [-l] dt nsteps file\n";
+	return 1;
+      }
+    }
+  }
+
+  if(argc - optind != 3) {
+    std::cerr << "Usage: TestEnergy [-l] dt nsteps file\n";
     return 1;
   }
 
-  const double dt = atof(argv[1]);
-  const unsigned nsteps = atoi(argv[2]);
+  const double dt = atof(argv[optind]);
+  const unsigned nsteps = atoi(argv[optind+1]);
 
   SuperstarCluster cluster(1);
 
-  ReadCluster(cluster, argv[3]);
+  ReadCluster(cluster, argv[optind+2]);
 
-  Steppers::Leapfrog stepper(new Steppers::Free, new Steppers::Interaction);
+  Stepper *const stepper = use_leapfrog ?
+    static_cast<Stepper*>(new Steppers::Leapfrog(new Steppers::Free,
+						 new Steppers::Interaction)) :
+    static_cast<Stepper*>(new
+			  Steppers::ModifiedEuler(new Steppers::Free,
+						  new Steppers::Interaction));
 
   std::cout.precision(16);
   std::cout << "# time\tenergy\n";
@@ -40,7 +62,7 @@ int main(int argc, char **argv) {
   PrintCluster(t, cluster);
   for(unsigned step=0;step<nsteps;step++) {
     t += dt;
-    stepper.Step(cluster, dt);
+    stepper->Step(cluster, dt);
     cluster.PrepareStep();
     PrintCluster(t, cluster);
   }
