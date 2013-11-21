4dc3d282425e1d02e0ecf238f7f2f2ec52f53bf7
William Throwe <wtt6@cornell.edu>
2013-11-21 02:06:52 -0500

    Allow dumping cluster state at several times

--- EvolveCluster.cpp
+++ EvolveCluster.cpp
@@ -7,7 +7,9 @@
 
 #include <cmath>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <string>
 #include <unistd.h>
 
 int main(int argc, char **argv) {
@@ -20,20 +22,18 @@ int main(int argc, char **argv) {
 	use_leapfrog = true;
 	break;
       default:
-	std::cerr << "Usage: EvolveCluster [-l] file t dt\n";
+	std::cerr << "Usage: EvolveCluster [-l] file dt t...\n";
 	return 1;
       }
     }
   }
 
-  if(argc - optind != 3) {
-    std::cerr << "Usage: EvolveCluster [-l] file t dt\n";
+  if(argc - optind < 3) {
+    std::cerr << "Usage: EvolveCluster [-l] file dt t...\n";
     return 1;
   }
 
-  const double tmax = atof(argv[optind+1]);
-  const double dt = atof(argv[optind+2]);
-  const unsigned nsteps = static_cast<unsigned>(round(tmax/dt));
+  const double dt = atof(argv[optind+1]);
 
   SuperstarCluster cluster(1);
   ReadCluster(cluster, argv[optind]);
@@ -45,11 +45,22 @@ int main(int argc, char **argv) {
 			  Steppers::ModifiedEuler(new Steppers::Free,
 						  new Steppers::Interaction));
 
-  for(unsigned step=0;step<nsteps;step++) {
+  int argnum = optind + 2;
+  double goal_t = atof(argv[argnum]);
+  for(unsigned step=0;;step++) {
     stepper->Step(cluster, dt);
+    if(step * dt >= goal_t) {
+      cluster.PrepareStep();
+      std::string filename = ".cluster";
+      filename = argv[argnum] + filename;
+      std::ofstream stream(filename.c_str());
+      stream.precision(16);
+      WriteCluster(cluster, stream);
+      if(++argnum >= argc) {
+	return 0;
+      }
+      goal_t = atof(argv[argnum]);
+    }
   }
-  cluster.PrepareStep();
-  std::cout.precision(16);
-  WriteCluster(cluster, std::cout);
   return 0;
 }
